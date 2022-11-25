42738ab280f532ad0d425e0eaa09d5f9e32d6cd6
Kapturov Alexander <86742866+Ultraluxe25@users.noreply.github.com>
2022-11-25 15:45:09 +0300

    first attempt to solve

--- Python/find longest (shortest) sentence.py
+++ Python/find longest (shortest) sentence.py
@@ -1,44 +1,42 @@
-"""
-Дан файл состоящий из нескольких предложений. Нужно найти длину наименьшего и наибольшего предложение и вывести их длины и сами предложения
-"""
-
-intermediate_sentences = []
-final_sentences = []
-
-
 def remove_blank_string(text):
     while '' in text:
         text.remove('')
     return text
 
 
-def function_separator(text, sign):
-    if sign in text:
-        text = text.replace(sign, '+' + sign).split(sign)
-        for single_sentence in text:
-            intermediate_sentences.append(single_sentence.replace('\n', '').replace('+', sign).strip())
+def function_separator(text): pass
 
 
 with open('genii_Makarenko_1.txt', 'r', encoding='UTF-8') as file:
     file = file.read()
     sentences = file.split('\n')
 
-    for sentence in sentences:
-        function_separator(sentence, '!')
-        function_separator(sentence, '?')
-        if '!' not in sentence and '?' not in sentence:
-            intermediate_sentences.append(sentence.strip())
-        # if '?' in sentence:
-        #     inner_sentence = sentence.replace('?', '+?').split('?')
-        #     for single_sentence in inner_sentence:
-        #         intermediate_sentences.append(single_sentence.replace('\n', '').replace('+', '?').strip())
-        # elif '!' in sentence:
-        #     inner_sentence = sentence.replace('!', '+!').split('!')
-        #     for single_sentence in inner_sentence:
-        #         intermediate_sentences.append(single_sentence.replace('\n', '').replace('+', '!').strip())
-        # else:
-        #     intermediate_sentences.append(sentence.strip())
+    intermediate_sentences = []
+    final_sentences = []
 
+    for sentence in sentences:
+        # sentence = sentence[:-1]  # remove '\n' sign
+        if '?' in sentence:
+            # sentence = sentence.replace('?', '+?')
+            inner_sentence = sentence.replace('?', '+?').split('?')
+            for single_sentence in inner_sentence:
+                # single_sentence = single_sentence.replace('\n', '').replace('+', '?')
+                # single_sentence = single_sentence.replace('+', '?')
+                # intermediate_sentences.append(single_sentence.strip())
+                intermediate_sentences.append(single_sentence.replace('\n', '').replace('+', '?').strip())
+        elif '!' in sentence:
+            # sentence = sentence.replace('!', '+!')
+            inner_sentence = sentence.replace('!', '+!').split('!')
+            for single_sentence in inner_sentence:
+                # single_sentence = single_sentence.replace('\n', '').replace('+', '!')
+                # single_sentence = single_sentence.replace('+', '!')
+                intermediate_sentences.append(single_sentence.replace('\n', '').replace('+', '!').strip())
+                # intermediate_sentences.append(single_sentence.strip())
+        else:
+            intermediate_sentences.append(sentence.strip())
+
+    # while '' in intermediate_sentences:
+    #     intermediate_sentences.remove('')
     intermediate_sentences = remove_blank_string(intermediate_sentences)
 
     for i in intermediate_sentences:
