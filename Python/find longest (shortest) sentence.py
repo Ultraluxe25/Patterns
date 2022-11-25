5ba603bee60b9733eaa6604005dc76812ff902d5
Kapturov Alexander <86742866+Ultraluxe25@users.noreply.github.com>
2022-11-25 15:22:04 +0300

    Create find longest (shortest) sentence.py

---
+++ Python/find longest (shortest) sentence.py
@@ -0,0 +1,40 @@
+intermediate_sentences = []
+final_sentences = []
+
+
+def remove_blank_string(text):
+    while '' in text:
+        text.remove('')
+    return text
+
+
+def function_separator(text, sign):
+    if sign in text:
+        inner_sentence = text.replace(sign, '+' + sign).split(sign)
+        for single_sentence in inner_sentence:
+            intermediate_sentences.append(single_sentence.replace('\n', '').replace('+', sign).strip())
+
+
+with open('genii_Makarenko_1.txt', 'r', encoding='UTF-8') as file:
+    file = file.read()
+    sentences = file.split('\n')
+
+    for sentence in sentences:
+        function_separator(sentence, '!')
+        function_separator(sentence, '?')
+        intermediate_sentences.append(sentence.strip())
+        # if '?' in sentence:
+        #     inner_sentence = sentence.replace('?', '+?').split('?')
+        #     for single_sentence in inner_sentence:
+        #         intermediate_sentences.append(single_sentence.replace('\n', '').replace('+', '?').strip())
+        # elif '!' in sentence:
+        #     inner_sentence = sentence.replace('!', '+!').split('!')
+        #     for single_sentence in inner_sentence:
+        #         intermediate_sentences.append(single_sentence.replace('\n', '').replace('+', '!').strip())
+        # else:
+        #     intermediate_sentences.append(sentence.strip())
+
+    intermediate_sentences = remove_blank_string(intermediate_sentences)
+
+    for i in intermediate_sentences:
+        print(i)
