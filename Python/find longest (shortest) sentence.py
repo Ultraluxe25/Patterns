29cd5cb18f918e16bbbee890601c16d0f03d0996
Kapturov Alexander <86742866+Ultraluxe25@users.noreply.github.com>
2022-11-26 15:39:38 +0300

    Update find longest (shortest) sentence.py

--- Python/find longest (shortest) sentence.py
+++ Python/find longest (shortest) sentence.py
@@ -1,43 +1,40 @@
+intermediate_sentences = []
+final_sentences = []
+
+
 def remove_blank_string(text):
     while '' in text:
         text.remove('')
     return text
 
 
-def function_separator(text): pass
+def function_separator(text, sign):
+    if sign in text:
+        text = text.replace(sign, '+' + sign).split(sign)
+        for single_sentence in text:
+            intermediate_sentences.append(single_sentence.replace('\n', '').replace('+', sign).strip())
+
+
+def dot_separator(intermediate_sentences, sign):
+    for sentence in intermediate_sentences:
+        if sign in sentence:  # and (not sentence[sentence.index(sign) - 1].isupper()):
+            sentence = sentence.replace(sign, '+' + sign).split(sign)
+            for single_sentence in sentence:
+                final_sentences.append(single_sentence.replace('\n', '').replace('+', sign).strip())
 
 
 with open('genii_Makarenko_1.txt', 'r', encoding='UTF-8') as file:
     file = file.read()
     sentences = file.split('\n')
 
-    intermediate_sentences = []
-    final_sentences = []
-
     for sentence in sentences:
-        # sentence = sentence[:-1]  # remove '\n' sign
-        if '?' in sentence:
-            # sentence = sentence.replace('?', '+?')
-            inner_sentence = sentence.replace('?', '+?').split('?')
-            for single_sentence in inner_sentence:
-                # single_sentence = single_sentence.replace('\n', '').replace('+', '?')
-                # single_sentence = single_sentence.replace('+', '?')
-                # intermediate_sentences.append(single_sentence.strip())
-                intermediate_sentences.append(single_sentence.replace('\n', '').replace('+', '?').strip())
-        elif '!' in sentence:
-            # sentence = sentence.replace('!', '+!')
-            inner_sentence = sentence.replace('!', '+!').split('!')
-            for single_sentence in inner_sentence:
-                # single_sentence = single_sentence.replace('\n', '').replace('+', '!')
-                # single_sentence = single_sentence.replace('+', '!')
-                intermediate_sentences.append(single_sentence.replace('\n', '').replace('+', '!').strip())
-                # intermediate_sentences.append(single_sentence.strip())
-        else:
-            intermediate_sentences.append(sentence.strip())
+        function_separator(sentence, '!')
+        function_separator(sentence, '?')
+        dot_separator(intermediate_sentences, '.')
+        # if '!' not in sentence and '?' not in sentence:
+        #     intermediate_sentences.append(sentence.strip())
 
-    # while '' in intermediate_sentences:
-    #     intermediate_sentences.remove('')
-    intermediate_sentences = remove_blank_string(intermediate_sentences)
+    final_sentences = remove_blank_string(final_sentences)
 
-    for i in intermediate_sentences:
+    for i in final_sentences:
         print(i)
